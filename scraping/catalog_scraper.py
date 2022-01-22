09001cca3c529304508a19f0395e613782a93975
Arnob Kazi <shahirmuktasid22@gmail.com>
2022-01-22 03:11:55 -0500

    updates

--- scraping/catalog_scraper.py
+++ scraping/catalog_scraper.py
@@ -5,5 +5,9 @@ with open('entries.html', 'r') as file:
 
 soup = BeautifulSoup(data, 'html.parser')
 newdata = soup.findAll("td", {"class" : "nttitle"})
-
-requests.get("https://selfservice.mypurdue.purdue.edu/prod/bwckctlg.p_disp_course_detail?cat_term_in=202310&subj_code_in=WGSS&crse_numb_in=68100")
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.1.2222.33 Safari/537.36",
+    "Accept-Encoding": "*",
+    "Connection": "keep-alive"
+}
+requests.get("https://selfservice.mypurdue.purdue.edu/prod/bwckctlg.p_disp_course_detail?cat_term_in=202310&subj_code_in=WGSS&crse_numb_in=68100", headers)
