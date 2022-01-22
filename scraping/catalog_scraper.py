f9c19dce9486034f450fe42e3c5e6c424216ee5e
Arnob Kazi <shahirmuktasid22@gmail.com>
2022-01-22 06:34:52 -0500

    some progress

--- scraping/catalog_scraper.py
+++ scraping/catalog_scraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
+
+
 with open('entries.html', 'r') as file:
     data = file.read()
 
@@ -10,10 +12,12 @@ newdata = soup.findAll("td", {"class" : "nttitle"})
 
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.1.2222.33 Safari/537.36",
     "Accept-Encoding": "*",
     "Connection": "keep-alive"
 }
+
+
+
 t = requests.get("https://selfservice.mypurdue.purdue.edu/prod/bwckctlg.p_disp_course_detail?cat_term_in=202310&subj_code_in=WGSS&crse_numb_in=68100", headers=headers)
 pprint(t.content)
 
