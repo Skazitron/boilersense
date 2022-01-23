7134aad44fc6fe815a2c711db90c9ac713b90ec2
Avi Patel <avidpatel1@gmail.com>
2022-01-23 01:19:58 -0500

    rendering stuff

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 from flask import jsonify
 
+
 app = Flask(__name__)
+CORS(app)
 jsonfile = open("idData.json")
 json = json.load(jsonfile)
 
