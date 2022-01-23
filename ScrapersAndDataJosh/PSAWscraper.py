209de77468e1664517f7d2b7ed889342f2d14a91
joshkleyman <jkleyman@purdue.edu>
2022-01-23 00:06:38 -0500

    Full reddit comment retrieval

--- ScrapersAndDataJosh/PSAWscraper.py
+++ ScrapersAndDataJosh/PSAWscraper.py
@@ -1,3 +1,4 @@
+from numpy import array
 import praw
 import pandas as pd
 import datetime as dt
@@ -8,6 +9,15 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import string
+
+def addCoursesToBInfo():
+    with open("./course_binfo.txt", "r") as f:
+        with open("./course_binfo_compressed.txt", "w") as f2:
+            for line in f:
+                strippedLine = line.split(' - ')[0].replace(" ", "")
+                if re.search(r"^CS[0-3]\d+", strippedLine) or re.search(r"^EAPS[0-3]\d+", strippedLine):
+                    f2.write(strippedLine + '\n')
+                    print("Added ", strippedLine)
 
 def soupScrape():
     URL = "https://www.thesaurus.com/browse/easy"
@@ -22,50 +32,45 @@ def soupScrape():
 
 def redditScrape():
 
-    def findSynonyms():
-        for synonym in synonyms:
-            index = comment.body.lower().find(synonym)
-            if index == -1 or (index != 0 and comment.body[index-1] not in string.whitespace + string.punctuation) or (index + len(synonym) < len(comment.body) and comment.body[index + len(synonym)] not in string.whitespace + string.punctuation):
-                continue
-            else:
-                comments.append(comment.body)
-                print(synonym, comment.body, comment.body.lower().find(synonym))
-                return 1
-        return 0
+    courseArr = []
+    with open("./course_binfo_compressed.txt") as f:
+        courseArr = f.readlines()
+        courseArr = [course.rstrip() for course in courseArr]
 
-    start_epoch=int(dt.datetime(2021, 11, 1).timestamp())
+    start_epoch=int(dt.datetime(2015, 1, 1).timestamp())
 
-    comments = []
+    df = pd.DataFrame(columns=['Course number','Text'])
+    for course in courseArr:
+        for submission in api.search_submissions(
+            after=start_epoch,
+            subreddit='Purdue',
+            q=f'({course[:-2]}|{course})',
+            limit=10000
+        ):
+            print(submission.title)
+            if len(re.compile("\\D{2,4}\\d{3,5}").findall(submission.title)) == 1:
+                for comment in api.search_comments(
+                    subreddit='Purdue',
+                    link_id=submission.id,
+                    filter=['body'],
+                    sort_type='score'
+                ):
+                    if comment.score < 6:
+                        continue
+                    for synonym in synonyms:
+                        index = comment.body.lower().find(synonym)
+                        if not (index == -1 or (index != 0 and comment.body[index-1] not in string.whitespace + string.punctuation) or (index + len(synonym) < len(comment.body) and comment.body[index + len(synonym)] not in string.whitespace + string.punctuation)):
+                            df = df.append({"Course number": course, "Text": comment.body }, ignore_index=True)
+                            print(comment)
+                            break
+                    time.sleep(0.15)
+            time.sleep(1)
 
-    print("Starting reddit crawl...")
-
-    course = '\"ma261 curve\"'
-
-    for submission in api.search_submissions(
-        after=start_epoch,
-        subreddit='Purdue',
-        q=course,
-        limit=10000
-    ):
-        print(submission.title)
-        if len(re.compile("\\D{2,4}\\d{3,5}").findall(submission.title)) == 1:
-            for comment in api.search_comments(
-                subreddit='Purdue',
-                link_id=submission.id,
-                filter=['body']
-            ):
-                findSynonyms()
-                time.sleep(0.1)
-        time.sleep(1)
-
-    # df = pd.DataFrame(columns=['comments'])
-    # df['comments'] = comments
-    # strippedCourse = re.sub(r'\W+', '', course)
-    # if (os.name == 'nt'):
-    #     csv = f'./ScrapersAndDataJosh\\{strippedCourse}_comments.csv'
-    # else:
-    #     csv = f'./ScrapersAndDataJosh/{strippedCourse}_comments.csv'
-    # df.to_csv(csv)
+    if (os.name == 'nt'):
+        csv = f'./ScrapersAndDataJosh\\reddit_comments.csv'
+    else:
+        csv = f'./ScrapersAndDataJosh/reddit_comments.csv'
+    df.to_csv(csv)
 
 
 if __name__ == '__main__':
@@ -76,6 +81,7 @@ if __name__ == '__main__':
     with open("./ScrapersAndDataJosh\\synonyms.txt") as f:
         synonyms = f.readlines()
         synonyms = [synonym.rstrip() for synonym in synonyms]
+            
         
     reddit = praw.Reddit(
         client_id=client_id[:-1],
