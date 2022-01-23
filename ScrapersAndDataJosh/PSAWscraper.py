eed46a5fa44efb4b76e093e421e6adebd5a54482
joshkleyman <jkleyman@purdue.edu>
2022-01-22 20:05:44 -0500

    Reddit scraper 2.0

--- ScrapersAndDataJosh/PSAWscraper.py
+++ ScrapersAndDataJosh/PSAWscraper.py
@@ -4,11 +4,73 @@ import datetime as dt
 import os
 from psaw import PushshiftAPI
 import re
+import time
+import requests
+from bs4 import BeautifulSoup
+import string
+
+def soupScrape():
+    URL = "https://www.thesaurus.com/browse/easy"
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+    #print(soup.prettify())
+
+    table = soup.find('ul', class_='css-123gjy7 e1ccqdb60').find_all('li')
+    for val in table:
+        print(val.find('a').text)
+
+
+def redditScrape():
+    start_epoch=int(dt.datetime(2010, 1, 1).timestamp())
+
+    comments = []
+
+    def findSynonyms():
+        for synonym in synonyms:
+            index = comment.body.lower().find(synonym)
+            if index == -1 or (index != 0 and comment.body[index-1] not in string.whitespace + string.punctuation) or (index + len(synonym) < len(comment.body) and comment.body[index + len(synonym)] not in string.whitespace + string.punctuation):
+                continue
+            else:
+                comments.append(comment.body)
+                print(synonym, comment.body, comment.body.lower().find(synonym))
+                return 1
+        return 0
+
+    print("Starting reddit crawl...")
+
+    for submission in api.search_submissions(
+        after=start_epoch,
+        subreddit='Purdue',
+        q="MA261|ma261|Ma261",
+        limit=10000
+    ):
+        if len(re.compile("\\D{2,4}\\d{3,5}").findall(submission.title)) == 1:
+            for comment in api.search_comments(
+                subreddit='Purdue',
+                link_id=submission.id,
+                filter=['body']
+            ):
+                findSynonyms()
+                time.sleep(0.1)
+        time.sleep(1)
+
+    df = pd.DataFrame(columns=['comments'])
+    df['comments'] = comments
+    if (os.name == 'nt'):
+        csv = f'./ScrapersAndDataJosh\\PSAWcomments.csv'
+    else:
+        csv = f'./ScrapersAndDataJosh/PSAWcomments.csv'
+    df.to_csv(csv)
+
 
 if __name__ == '__main__':
     # Get credentials from DEFAULT instance in praw.ini
     with open("./ScrapersAndDataJosh\\PRAW\\secrets.txt","r") as f:
         client_id, client_secret, user_agent, username, password = f.readlines()    #client_id, client_secret, user_agent, username, password
+
+    with open("./ScrapersAndDataJosh\\synonyms.txt") as f:
+        synonyms = f.readlines()
+        synonyms = [synonym.rstrip() for synonym in synonyms]
         
     reddit = praw.Reddit(
         client_id=client_id[:-1],
@@ -20,21 +82,4 @@ if __name__ == '__main__':
 
     api = PushshiftAPI(reddit)
 
-    start_epoch=int(dt.datetime(2021, 11, 30).timestamp())
-
-    i = 0
-    for submission in api.search_submissions(
-        after=start_epoch,
-        subreddit='Purdue',
-        q='MA261 and FYE',
-        limit=1
-    ):
-        if len(re.compile("\\D{2,4}\\d{0,5}").findall(submission.title)) == 1:
-            for comment in api.search_comments(
-                subreddit='Purdue',
-                link_id=submission.id,
-                filter=['body']
-            ):
-                print(comment.body)
-                i += 1
-    print(i)
+    redditScrape()
